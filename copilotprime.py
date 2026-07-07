15b4a5b50b54404d8810227b45d0f6b07a4f6e42
Lily Nishizawa <lilynishizawa@gmail.com>
2026-07-07 16:04:20 -0700

    Use Sieve of Eratosthenes in copilotprime.py

--- copilotprime.py
+++ copilotprime.py
@@ -1,17 +1,35 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+import math
 
-primes = []
-num = 2
-while len(primes) < 20:
-    if is_prime(num):
-        primes.append(num)
-    num += 1
 
-for prime in primes:
-    print(prime)
+def sieve_of_eratosthenes(limit):
+    sieve = [True] * (limit + 1)
+    sieve[0:2] = [False, False]
+
+    for i in range(2, int(limit**0.5) + 1):
+        if sieve[i]:
+            for multiple in range(i * i, limit + 1, i):
+                sieve[multiple] = False
+
+    return [i for i, is_prime in enumerate(sieve) if is_prime]
+
+
+def first_n_primes(n):
+    if n <= 0:
+        return []
+    if n == 1:
+        return [2]
+
+    estimate = int(n * (math.log(n) + math.log(math.log(n)))) + 10
+    primes = sieve_of_eratosthenes(estimate)
+
+    while len(primes) < n:
+        estimate *= 2
+        primes = sieve_of_eratosthenes(estimate)
+
+    return primes[:n]
+
+
+if __name__ == "__main__":
+    target = 20
+    for prime in first_n_primes(target):
+        print(prime)
