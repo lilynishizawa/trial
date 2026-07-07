bb8758a6f2add5c8c13738e8d53967a7af10aaeb
Lily Nishizawa <lilynishizawa@gmail.com>
2026-07-07 15:27:49 -0700

    asked copilot to write program

---
+++ copilotprime.py
@@ -0,0 +1,17 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+primes = []
+num = 2
+while len(primes) < 20:
+    if is_prime(num):
+        primes.append(num)
+    num += 1
+
+for prime in primes:
+    print(prime)
